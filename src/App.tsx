ba32cbc4646cf690779db57548171f9d8607d56f
unknown <chandanpms@gmail.com>
2025-05-07 16:17:14 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import React from "react";
 
 function App() {
   return (
