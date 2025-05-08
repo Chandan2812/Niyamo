26b85e74a68e06b1dd5962449eb6271c86455058
unknown <chandanpms@gmail.com>
2025-05-08 17:48:13 +0530

    updated nc concepts and added theme

--- src/pages/nc_concepts/Bkc.tsx
+++ src/pages/nc_concepts/Bkc.tsx
@@ -4,9 +4,9 @@ import Footer from "../../components/Footer";
 
 const Bkc: React.FC = () => {
   return (
-    <div>
+    <div className="bg-white text-black font-raleway dark:bg-black dark:text-white  font-light dark:font-thin">
       <Navbar />
-      <section className="px-4 md:px-12 lg:px-24  text-[#003D5B] py-28">
+      <section className="w-11/12 mx-auto py-28 px-4 md:px-8">
         {/* Title */}
         <h1 className="text-3xl md:text-4xl font-semibold text-center mb-12">
           Biryani Kebab Chai
@@ -18,17 +18,15 @@ const Bkc: React.FC = () => {
             Bringing authentic Awadhi delights to London
           </h2>
           <p className="max-w-3xl text-sm leading-relaxed mb-4">
-            At BKC -– Biryani Kebab Chai, our story is one of passion,
+            At BKC – Biryani Kebab Chai, our story is one of passion,
             exploration, and the desire to share the unparalleled tastes of
-            Awadhi cuisine with London. Launched in Marble Arch in 2022; and in
+            Awadhi cuisine with London. Launched in Marble Arch in 2022 and in
             Soho in 2023, our mission remains the same: to celebrate the
-            culinary wonders of Lucknow. BKC is on a journey to bring the
-            essence of the Desi Delicacies and the legendary Degh to
-            Dastarkhwan, evoking a feeling of joy and celebration to your plate.
+            culinary wonders of Lucknow...
           </p>
           <a
             href="https://bkclondon.uk/"
-            className="text-blue-500 text-sm "
+            className="text-[var(--primary-color)] text-sm font-light"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -37,17 +35,17 @@ const Bkc: React.FC = () => {
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-6">
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/Slider-1-1.png"
-              alt="Cipriani concept 1"
+              alt="BKC concept 1"
               className="w-full h-full object-cover rounded"
             />
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/2-1.png"
-              alt="Cipriani concept 2"
+              alt="BKC concept 2"
               className="w-full h-full object-cover rounded"
             />
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/3-1.png"
-              alt="Cipriani concept 3"
+              alt="BKC concept 3"
               className="w-full h-full object-cover rounded"
             />
           </div>
@@ -61,11 +59,6 @@ const Bkc: React.FC = () => {
               alt="Cuisine 1"
               className="w-full h-full object-cover rounded"
             />
-            {/* <img
-              src="https://images.squarespace-cdn.com/content/v1/6048e88f29a3c733e02afa4f/e5a6a702-65fc-4e72-a12f-bd5d83429dbd/IMG_8063.JPG?format=1000w"
-              alt="Cuisine 2"
-              className="w-full h-full object-cover rounded"
-            /> */}
           </div>
           <div>
             <h2 className="text-2xl font-semibold mb-4">
@@ -73,15 +66,8 @@ const Bkc: React.FC = () => {
             </h2>
             <p className="text-sm leading-relaxed">
               At BKC the dishes are thoughtfully curated by our Head Chef – Arsh
-              Thakur, following the age old Awadhi cooking techniques using the
-              freshest British produce, sourced from the local farms and brought
-              straight to your tables. You’l find the soul of Lucknow in our
-              expertly prepared Lucknowi Biryani, succulent North Indian kebabs,
-              and the comforting warmth of our aromatic chai. Whether you’re in
-              search of the best Biryani near you or the best Kathi roll in
-              London, BKC London is your gateway to the finest Indian cuisine,
-              making it a standout Indian restaurant in Soho and Mayfair,
-              London.
+              Thakur, following the age-old Awadhi cooking techniques using the
+              freshest British produce...
             </p>
           </div>
         </div>
@@ -91,28 +77,23 @@ const Bkc: React.FC = () => {
           <h2 className="text-2xl font-semibold mb-4">Design</h2>
           <p className="text-sm leading-relaxed mb-6 max-w-3xl">
             Charles Street has character and history, which we are keen to
-            preserve and enhance. The building at 16 Charles Street is a Grade
-            II* listed Georgian property, which dates back to 1753. The building
-            has played a significant role in the social history of London and
-            was the former home of the high society hostess Dame Margaret
-            Greville. Here, Dame Margaret hosted society soirees where she
-            entertained politicians, royalty and celebrities.
+            preserve and enhance...
           </p>
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/IMG2.jpg"
               alt="Design 1"
-              className=" h-full object-cover rounded"
+              className="h-full object-cover rounded"
             />
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/Home-replace3-1.png"
               alt="Design 2"
-              className=" h-full object-cover rounded"
+              className="h-full object-cover rounded"
             />
             <img
               src="https://bkclondon.uk/wp-content/uploads/2024/06/attachment-1.png"
-              alt="Design 2"
-              className=" h-full object-cover rounded"
+              alt="Design 3"
+              className="h-full object-cover rounded"
             />
           </div>
         </div>
@@ -127,7 +108,7 @@ const Bkc: React.FC = () => {
               allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-              title="7 Edgware Rd, Tyburnia, London W2 2ER, United Kingdom"
+              title="BKC Marble Arch"
             ></iframe>
           </div>
         </div>
